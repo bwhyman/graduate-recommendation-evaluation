4f6db8cfaf460258cfaea7634ad9a91a89d4eef7
BO <bwang0629@gmail.com>
2025-08-14 14:28:11 +0800

    refactor: category weighting

--- src/main/java/cn/nefu/ccec/graduaterecommendationevaluation/dto/UserInfoDTO.java
+++ src/main/java/cn/nefu/ccec/graduaterecommendationevaluation/dto/UserInfoDTO.java
@@ -12,6 +12,6 @@ import lombok.NoArgsConstructor;
 public class UserInfoDTO {
     private String name;
     private String collName;
-    private String catNames;
+    private String categories;
     private String majorName;
 }
--- src/main/java/cn/nefu/ccec/graduaterecommendationevaluation/repository/UserRepository.java
+++ src/main/java/cn/nefu/ccec/graduaterecommendationevaluation/repository/UserRepository.java
@@ -14,7 +14,9 @@ public interface UserRepository extends ReactiveCrudRepository<User, Long> {
     Mono<User> findByAccount(String account);
 
     @Query("""
-            select t1.id, t1.name, t2.name as coll_name, JSON_ARRAYAGG(t4.name) as cat_names, t5.name as major_name
+            select t1.id, t1.name, t2.name as coll_name,
+            json_arrayagg(json_insert(t4.weighting, '$.id', cast(t4.id as char), '$.name', t4.name)) as categories,
+            t5.name as major_name
             from user t1
                      left join college t2 on t1.coll_id = t2.id
                      left join user_category t3 on t1.id = t3.user_id
